74a421affa0e274583974d2821e028cde6d32ad5
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-03-06 08:28:41 +1000

    chore: wip

--- src/test.js
+++ src/test.js
@@ -27,6 +27,7 @@ const getUnspent = async (address) => {
 const getAUtxo = async (txid, to, amount) => {
   // const url = `https://api.blockcypher.com/v1/btc/test3/txs/${txid}`;
   const url = `https://blockstream.info/testnet/api/tx/${txid}`;
+  console.log(url);
   const response = await axios.get(url);
 
   const utxos = response.data.vout;
@@ -34,6 +35,16 @@ const getAUtxo = async (txid, to, amount) => {
     (vout) => vout.value >= amount && vout.scriptpubkey_address === to
   );
   return utxo;
+};
+
+const getAUtxoAsHex = async (txid) => {
+  // const url = `https://api.blockcypher.com/v1/btc/test3/txs/${txid}`;
+  const url = `https://blockstream.info/testnet/api/tx/${txid}/hex`;
+  console.log(url);
+  const response = await axios.get(url);
+
+  console.log(response.data);
+  return response.data;
 };
 
 const test = async () => {
@@ -44,6 +55,8 @@ const test = async () => {
   const seed = bip39.mnemonicToSeedSync(mnemonic);
   const root = bip32.fromSeed(seed, network);
   const child = root.derivePath(path);
+
+  const wif = child.toWIF();
   const privateKeyBuffer = child.privateKey;
 
   const address = bitcoin.payments.p2wpkh({
@@ -55,13 +68,16 @@ const test = async () => {
   // tb1q26y7u4jw3canmy3g637tna7qpr0degnzvv0fh0
   console.log(address.address);
 
+  // https://github.com/bitcoinjs/bitcoinjs-lib/issues/1565
   // example https://bitcoin.stackexchange.com/questions/118945/how-to-build-a-transaction-using-bitcoinjs-lib
   const psbt = new bitcoin.Psbt({ network: network });
 
   const from = "tb1q26y7u4jw3canmy3g637tna7qpr0degnzvv0fh0";
   const unspent = await getUnspent(from);
+  console.log(unspent);
 
   const utxo = await getAUtxo(unspent.tx_hash, from, 1000);
+  console.log(utxo);
 
   //   psbt.addInput({
   //     hash: unspent.tx_hash,
@@ -76,9 +92,25 @@ const test = async () => {
   //     redeemScript: Buffer.from(utxo.scriptpubkey, "hex"),
   //   });
 
+//   const output_script = bitcoin.payments.p2wpkh({
+//     pubkey: pubKey, network: network
+// }).output
+
+  // psbt.addInput({
+  //   hash: input.txid,
+  //   index: input.vout,
+  //   // witnessUtxo: {script: output_script , value: input.value }
+  //   nonWitnessUtxo: Buffer.from(input.txHex, 'hex'),
+  // })
+
+  const utxoHex = await getAUtxoAsHex(unspent.tx_hash);
+  // console.log(utxoHex);
+
+  // https://not-satoshi.com/unlocking-the-secrets-sending-raw-bitcoin-transactions-with-javascript-made-simple/
   const input = {
     hash: "1abe5599863a47355ce106dd13ec1108f31ce5dd7f9e2564546abc26bc5b420c",
     index: 0,
+    nonWitnessUtxo: Buffer.from(utxoHex, "hex"),
     // witnessUtxo: {
     //   script: Buffer.from(
     //     utxo.scriptpubkey,
@@ -99,34 +131,22 @@ const test = async () => {
   psbt.addOutput(output);
 
   const privateKeyAsHex = privateKeyBuffer.toString("hex");
-  // https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/transactions.spec.ts#L24C5-L26C7
-  const pk = ECPair.fromWIF(privateKeyAsHex, network);
 
-  psbt.signInput(0, pk);
-  psbt.finalizeInput(0);
-  //const tx = psbt.extractTransaction();
-  console.log(tx.toHex());
+
+  // const pk = ECPair.fromPrivateKey(privateKeyBuffer, network);
+  // https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/transactions.spec.ts#L24C5-L26C7
+  const pk = ECPair.fromWIF(wif, network);
+
+  // psbt.signInput(0, pk);
+  psbt.signAllInputs(pk);
+
+  // psbt.finalizeInput(0);
+  // //const tx = psbt.extractTransaction();
+  // console.log(tx.toHex());
 
   // const txid = await broadcast(txHex);
   // return txid;
 
-  // Convert the private key to WIF (Wallet Import Format) for easier use and readability
-  // const privateKeyWIF = bitcoin.ECPair.fromPrivateKey(privateKeyBuffer);
-
-  //   const account = new CryptoAccount(
-  //     Buffer.from(privateKeyBuffer, "hex", { network: testnet })
-  //   );
-
-  //   console.log(await account.address("BTC"));
-  //   const balance = await account.getBalance("BTC");
-
-  //   console.log(balance);
-  //   if (balance < amount) {
-  //     console.log("Insufficient funds");
-  //     return;
-  //   }
-
-  //   console.log(await account.send("BTC", to, amount));
 };
 
 test();
